863abc709b3a53a351671ecfa6166a94f0aac9c1
Austin Ricketts <austinricketts4@gmail.com>
2020-03-18 22:35:25 -0400

    got the timer interval function to work

---
+++ script.js
@@ -0,0 +1,18 @@
+var timeEl = document.querySelector(".time");
+
+var secondsLeft = 60;
+
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = "Timer: " + secondsLeft;
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+      }
+  
+    }, 1000);
+}
+
+setTime();
